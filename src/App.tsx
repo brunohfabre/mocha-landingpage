9b2c76d02784f1d9536e13152e4345223bc1a32e
= <=>
2022-07-27 22:58:57 -0300

    fix download link

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import './App.css'
-
 type HandleDownloadData = 'darwin';
-
 
 export function App() {
   function handleDownload(platform: HandleDownloadData): void {
-    if(platform === 'darwin') {
-      window.open('https://workdrive.zohoexternal.com/external/2dd7c8a6b16933ec26d039270a454215bda5b9157e26f4bfe0fd250a7de92b50');
-    }
+    window.open(`http://165.227.197.107:5555/download/${platform}`);
   }
 
   return (
